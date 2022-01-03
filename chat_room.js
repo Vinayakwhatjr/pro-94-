8c0eec64d72ad0d137d379d286b21de0393ca368
Vinayakwhatjr <82382484+Vinayakwhatjr@users.noreply.github.com>
2022-01-03 17:51:40 +0530

    Add files via upload

--- chat_room.js
+++ chat_room.js
@@ -25,13 +25,14 @@ const firebaseConfig = {
 
             firebase.database().ref("/").child(room_name).update({
                   purpose: "Adding Room Name"
-            });
       }
-
-function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
-       Room_names = childKey;
-      //Start code
-
-      //End code
-      });});}
-getData();
+,function getData() {firebase.database().ref("/").on('value',
+function(snapshot) {document.getElementById("output").innerHTML ="";snapshot.forEach(function(childSnapshot) {childKey = childSnapshot.key;
+Room_names = childKey;
+ //Start
+      console.log("Room Name - " + Room_names);
+       row = "<div class='room_name' id="+Room_names+" onclick='redirectToRoomName(this.id)' >#"+Room_names +"</div><hr>";
+       document.getElementById("output").innerHTML += row;
+//End code
+});});}
+ ,getData() )};
